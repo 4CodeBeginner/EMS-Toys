8ab4a593cbd89494f5b8005feffc93775b826a13
Nopal <141114024+4CodeBeginner@users.noreply.github.com>
2026-02-28 11:03:59 +0700

    Initial Laravel EMS Toys project

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Data Users</title>
+</head>
+<body>
+
+<h1>Data Users</h1>
+
+<table border="1" cellpadding="10">
+    <tr>
+        <th>ID</th>
+        <th>ID User</th>
+        <th>Name</th>
+        <th>Role</th>
+        <th>Created At</th>
+    </tr>
+
+    @foreach($users as $user)
+    <tr>
+        <td>{{ $user->id }}</td>
+        <td>{{ $user->id_user }}</td>
+        <td>{{ $user->name }}</td>
+        <td>{{ $user->role }}</td>
+        <td>{{ $user->created_at }}</td>
+    </tr>
+    @endforeach
+
+</table>
+
+</body>
+</html>
